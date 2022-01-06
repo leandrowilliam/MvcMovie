669006b2ae6e42ac0e6c71325f1ac01af2506bb6
Leandro William <kriptom@outlook.com>
2022-01-06 17:20:33 -0500

    Add search input

--- Migrations/20220106205203_InitialCreate.Designer.cs
+++ Migrations/20220106205203_InitialCreate.Designer.cs
@@ -4,43 +4,44 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MvcMovie.Data;
 
 #nullable disable
 
 namespace MvcMovie.Migrations
 {
-    [DbContext(typeof(MvcMovieContext))]
-    [Migration("20220106205203_InitialCreate")]
-    partial class InitialCreate
+  [DbContext(typeof(MvcMovieContext))]
+  [Migration("20220106205203_InitialCreate")]
+  partial class InitialCreate
+  {
+    protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
+      modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
 
-            modelBuilder.Entity("MvcMovie.Models.Movie", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity("MvcMovie.Models.Movie", b =>
+          {
+            b.Property<int>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("INTEGER");
 
-                    b.Property<string>("Genre")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Genre")
+                      .HasColumnType("TEXT");
 
-                    b.Property<decimal>("Price")
-                        .HasColumnType("TEXT");
+            b.Property<decimal>("Price")
+                      .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("ReleaseDate")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("ReleaseDate")
+                      .HasColumnType("TEXT");
 
-                    b.Property<string>("Title")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Title")
+                      .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.ToTable("Movie");
-                });
+            b.ToTable("Movie");
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
